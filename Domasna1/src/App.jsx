abb2038ca8119a517340f01aa4d91fcb2ba93b83
Aleksandar Stevkovski <astevkovski87@gmail.com>
2024-03-20 19:20:06 +0100

    domasna

---
+++ Domasna1/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2>Hello</h2>
+        <h3>My name is Aleksandar and I am MERN Stack Developer</h3>
+        <img src="/slika.png" alt="Profile picture" className="profile-img" />
+      </div>
+      <p className="read-the-docs">
+        Find more about my projects on{" "}
+        <a href="https://github.com/sasko1987">GitHub</a>
+      </p>
+    </>
+  );
+}
+
+export default App;
